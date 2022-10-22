e390d9d2853a9c2ad75db9c88f8aafc09b7808a5
Martha A <mladame43@gmail.com>
2022-10-22 13:31:15 -0700

    added initial contact form with user name, email, message input areas

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -3,9 +3,36 @@ import { send } from 'emailjs-com';
 import '../styles/Contact.js';
 
 function ContactForm() {
-    retun (
+    retun(
         <div className='contactContainer'>
-
+            {/* when user clicks submit, call onSubmit to send email */}
+            <form onSubmit={onSubmit}>
+                {/* User Name save as from_name */}
+                <input
+                    type='text'
+                    name='from_name'
+                    placeholder='Your name'
+                    value={toSend.from_name}
+                    onChange={handleChange}
+                />
+                {/* User Email save as reply_to */}
+                <input
+                    type='text'
+                    name='reply_to'
+                    placeholder='Your email'
+                    value={toSend.reply_to}
+                    onChange={handleChange}
+                />
+                {/* User Message */}
+                <input
+                    type='text'
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
+                />
+                <button type="submit"/>Submit<button />
+            </form>
         </div>
     );
 }

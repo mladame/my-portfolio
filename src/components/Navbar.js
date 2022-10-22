4a58e2cba1e381a1f8608330724a35bc6a7b03ab
Martha A <mladame43@gmail.com>
2022-10-22 01:27:08 -0700

    updated links in navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function BasicExample() {
+function MyNavbar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">My Name</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#AboutMe">About Me</NavDropdown.Item>
+                            <NavDropdown.Item href="#Portfolio">#Portfolio</NavDropdown.Item>
+                            <NavDropdown.Item href="#Contact">Contact Me</NavDropdown.Item>
+                            <NavDropdown.Item href="#Resume">My Resume</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
                                 Separated link
@@ -31,4 +28,4 @@ function BasicExample() {
     );
 }
 
-export default BasicExample;
+export default MyNavbar;

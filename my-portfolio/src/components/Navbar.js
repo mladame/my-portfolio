7316d2610bd88060af58590cdfdb5c5c326d21ab
Martha A <mladame43@gmail.com>
2022-10-22 01:55:43 -0700

    updated app.js and navbar.js, components now being called through navbar.js with tabs using react bootstrap

---
+++ my-portfolio/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+import AboutMe from "./AboutMe";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Resume from "./Resume";
+
+function MyNavbar() {
+    return (
+        <Tabs
+            defaultActiveKey="profile"
+            id="fill-tab-example"
+            className="mb-3"
+            fill
+        >
+            <Tab eventKey="home" title="About Me">
+                {/* <AboutMe /> */}
+            </Tab>
+            <Tab eventKey="profile" title="Portfolio">
+                {/* <Portfolio /> */}
+            </Tab>
+            <Tab eventKey="longer-tab" title="Contact Me">
+                {/* <Contact /> */}
+            </Tab>
+            <Tab eventKey="contact" title="Resume">
+                {/* <Resume /> */}
+            </Tab>
+        </Tabs>
+    );
+}
+
+export default MyNavbar;

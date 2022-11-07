f5d2b40852fe780f05115660a951a94778986121
Martha A <mladame43@gmail.com>
2022-11-06 16:34:43 -0800

    added formatting css for resume page

--- my-portfolio/src/components/Navigation.js
+++ my-portfolio/src/components/Navigation.js
@@ -10,7 +10,7 @@ import Resume from "./pages/Resume";
 function Navigation() {
     return (
         <Tabs
-            defaultActiveKey="contact"
+            defaultActiveKey="resume"
             id="fill-tab-example"
             className="mb-3 nav-tabs"
             fill>
